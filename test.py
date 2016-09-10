284acbad302f9278cf52b55b825a7ff6e0951b92
Martin Ulmschneider <spam2@ulmschneider.org>
2016-09-10 21:42:22 +0200

    \

--- test.py
+++ test.py
@@ -1,40 +1,46 @@
 #!/usr/bin/python3
 
-#TODO: trademark
+#TODO: trademark research
 
-#from Crypto.Hash import SHA256
-#from Crypto.Cipher import AES
 from Cryptodome.Cipher import AES
 from Cryptodome.Random import get_random_bytes
+from Crypto.Hash import SHA256
 
-key = get_random_bytes(16)
-data = "This is a test".encode('utf-8')
-cipher = AES.new(key, AES.MODE_EAX)
-ciphertext, tag = cipher.encrypt_and_digest(data)
-nonce = cipher.nonce
-print(ciphertext)
-print(tag)
+passphrase = "mykey"
 
-cipher = AES.new(key, AES.MODE_EAX, nonce)
-data = cipher.decrypt_and_verify(ciphertext, tag)
-print(data)
+def crypt(plaintext, key):
+    plaintext = plaintext.encode('utf-8')
+    cipher = AES.new(key, AES.MODE_EAX)
+    ciphertext, tag = cipher.encrypt_and_digest(data)
+    nonce = cipher.nonce
+    print(ciphertext)
+    print(tag)
+    return ciphertext, nonce, tag
 
-#file_out = open("encrypted.bin", "wb")
-#[ file_out.write(x) for x in (cipher.nonce, tag, ciphertext) ]
+def decrypt(ciphertext, key, nonce, tag):
+    cipher = AES.new(key, AES.MODE_EAX, nonce)
+    data = cipher.decrypt_and_verify(ciphertext, tag)
+    print(data)
 
 
-#hash = SHA256.new()
-#hash.update('message'.encode())
-#digest = hash.digest()
-#print(digest)
-#
-#
-## https://pycryptodome.readthedocs.io/en/latest/src/examples.html#encrypt-data-with-aes might be simpler
-#obj = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
-#message = "The answer is no"
-#ciphertext = obj.encrypt(message)
-#obj2 = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
-#plaintext = obj2.decrypt(ciphertext)
-#print(ciphertext)
-#print(plaintext)
-#print(message)
+def genkey(passphrase):
+    passphrase=passphrase.encode('utf-8')
+    key = get_random_bytes(32) #TODO: store
+    salt = get_random_bytes(32) #TODO: store
+    salted_pass = passphrase + salt
+   
+    h = SHA256.new()
+    h.update(salted_pass)
+    intermed_key = h.digest()
+
+    cipher = AES.new(key, AES.MODE_CBC)
+    for i in range(1, 100000): #TODO: store?(authenticated?)
+        intermed_key = cipher.encrypt(intermed_key)
+    print(intermed_key)
+    h.update(intermed_key)
+    final_key = h.digest()
+    print(final_key)
+
+
+#crypt('asdf', key)
+genkey(passphrase)
